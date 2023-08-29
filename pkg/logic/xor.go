063071aec384596ee6d5f7edc1c794035a46f424
Michael Senn <michael@morrolan.ch>
2023-08-29 21:38:38 +0200

    Implement repeating-key XOR.

--- pkg/logic/xor.go
+++ pkg/logic/xor.go
@@ -1,6 +1,8 @@
 package logic
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Xor calculates the logical XOR of two bytes slices of equal length.
 //
@@ -9,10 +11,23 @@ func Xor(a, b []byte) []byte {
 	if len(a) != len(b) {
 		panic(fmt.Sprintf("Got slices of different length: %d != %d", len(a), len(b)))
 	}
+
+	return RepeatingXor(a, b)
+}
+
+// RepeatingXor calculates the logical XOR of two byte slices of arbitrary
+// length. Access to the shorter of the two will wrap around automatically if
+// required.
+func RepeatingXor(a, b []byte) []byte {
 	out := make([]byte, len(a))
 
-	for i := 0; i < len(a); i += 1 {
-		out[i] = a[i] ^ b[i]
+	n := len(a)
+	if len(b) > len(a) {
+		n = len(b)
+	}
+
+	for i := 0; i < n; i += 1 {
+		out[i] = a[i%len(a)] ^ b[i%len(b)]
 	}
 
 	return out
